ea7deafb7f8509b62bbe4be65412446881bf5ff4
mattietea <mattcthomas@me.com>
2017-05-02 16:56:44 -0400

    reset homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import {TutorialPage} from "../pages/tutorial/tutorial";
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TutorialPage;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
